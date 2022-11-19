9e8f5d03eae304ecf2dfa7ce5dc547d32a37d919
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-11-19 05:35:20 +0000

    [sessions-new/create/destroy]Nagano-cake

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,19 +4,21 @@ class Public::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+    #ログイン後はpublic/items/indexに移動
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+    root_path
+  end
 
   protected
   # 退会しているかを判断するメソッド
