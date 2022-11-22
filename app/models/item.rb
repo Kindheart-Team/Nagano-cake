09ea91f79b52f1ed80b3fc2af330dbfe66758fdd
mitoma99 <114859605+mitoma99@users.noreply.github.com>
2022-11-22 12:13:32 +0000

    [public_items]nagano-cake

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,20 @@ class Item < ApplicationRecord
   has_many :cart_items
 
 
-  def get_image(width,height)
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/default-image.jpeg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image
+  #def get_image(width,height)
+  # unless image.attached?
+  #     file_path = Rails.root.join('app/assets/default-image.jpeg')
+  #     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  #   end
+  #   image
+ # end
+
+  def get_image(width, height)
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/default-image.jpeg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
   def add_tax_price
