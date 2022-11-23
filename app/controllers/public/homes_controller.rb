e469fb4362a55f4599472f0fc883f5204c8fcf6f
mitoma99 <114859605+mitoma99@users.noreply.github.com>
2022-11-23 05:23:29 +0000

    [public_items2]nagano-cake

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,15 @@
 class Public::HomesController < ApplicationController
   def top
+    @items = Item.all.order('id DESC').limit(4)
+    # ASCだと古い順でDESCで新着順
   end
 
   def about
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :is_active, :genre_id, :image)
   end
 end
