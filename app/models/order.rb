dbfc6a1c3941f3ea3e335c23478deba336c9ad71
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-11-24 08:17:14 +0000

    [orders-status]Nagano-cake

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,6 @@ class Order < ApplicationRecord
   has_many :items, through: :ordered_items
 
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { wait: 0, confilm: 1, making: 2, preparation: 3, shipped: 4 }
 
 end
